2ee5d857bfde0298900694cd57904f5258afa7e5
zhanglei <1535835796@qq.com>
2018-08-16 23:34:31 +0800

    示例

---
+++ Summer.CompetitiveTender.View/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace Summer.CompetitiveTender.View
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.skinTabControl1 = new CCWin.SkinControl.SkinTabControl();
+            this.skinTabPage1 = new CCWin.SkinControl.SkinTabPage();
+            this.skinTabPage2 = new CCWin.SkinControl.SkinTabPage();
+            this.skinTabPage3 = new CCWin.SkinControl.SkinTabPage();
+            this.skinPushPanel1 = new CCWin.SkinControl.SkinPushPanel();
+            this.pushPanelItem1 = new CCWin.SkinControl.PushPanelItem();
+            this.pushPanelItem2 = new CCWin.SkinControl.PushPanelItem();
+            this.pushPanelItem3 = new CCWin.SkinControl.PushPanelItem();
+            this.pushPanelItem4 = new CCWin.SkinControl.PushPanelItem();
+            this.pushPanelItem5 = new CCWin.SkinControl.PushPanelItem();
+            this.skinPictureBox1 = new CCWin.SkinControl.SkinPictureBox();
+            this.skinPictureBox2 = new CCWin.SkinControl.SkinPictureBox();
+            this.skinTabControl1.SuspendLayout();
+            this.skinTabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.skinPushPanel1)).BeginInit();
+            this.skinPushPanel1.SuspendLayout();
+            this.pushPanelItem5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.skinPictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.skinPictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // skinTabControl1
+            // 
+            this.skinTabControl1.Alignment = System.Windows.Forms.TabAlignment.Left;
+            this.skinTabControl1.AnimatorType = CCWin.SkinControl.AnimationType.HorizSlide;
+            this.skinTabControl1.CloseRect = new System.Drawing.Rectangle(2, 2, 12, 12);
+            this.skinTabControl1.Controls.Add(this.skinTabPage2);
+            this.skinTabControl1.Controls.Add(this.skinTabPage1);
+            this.skinTabControl1.Controls.Add(this.skinTabPage3);
+            this.skinTabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.skinTabControl1.HeadBack = null;
+            this.skinTabControl1.ImgTxtOffset = new System.Drawing.Point(0, 0);
+            this.skinTabControl1.ItemSize = new System.Drawing.Size(40, 90);
+            this.skinTabControl1.Location = new System.Drawing.Point(8, 30);
+            this.skinTabControl1.Multiline = true;
+            this.skinTabControl1.Name = "skinTabControl1";
+            this.skinTabControl1.PageArrowDown = ((System.Drawing.Image)(resources.GetObject("skinTabControl1.PageArrowDown")));
+            this.skinTabControl1.PageArrowHover = ((System.Drawing.Image)(resources.GetObject("skinTabControl1.PageArrowHover")));
+            this.skinTabControl1.PageCloseHover = ((System.Drawing.Image)(resources.GetObject("skinTabControl1.PageCloseHover")));
+            this.skinTabControl1.PageCloseNormal = ((System.Drawing.Image)(resources.GetObject("skinTabControl1.PageCloseNormal")));
+            this.skinTabControl1.PageDown = ((System.Drawing.Image)(resources.GetObject("skinTabControl1.PageDown")));
+            this.skinTabControl1.PageHover = ((System.Drawing.Image)(resources.GetObject("skinTabControl1.PageHover")));
+            this.skinTabControl1.PageImagePosition = CCWin.SkinControl.SkinTabControl.ePageImagePosition.Left;
+            this.skinTabControl1.PageNorml = null;
+            this.skinTabControl1.SelectedIndex = 2;
+            this.skinTabControl1.Size = new System.Drawing.Size(733, 424);
+            this.skinTabControl1.SizeMode = System.Windows.Forms.TabSizeMode.Fixed;
+            this.skinTabControl1.TabIndex = 0;
+            // 
+            // skinTabPage1
+            // 
+            this.skinTabPage1.BackColor = System.Drawing.Color.White;
+            this.skinTabPage1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.skinTabPage1.Location = new System.Drawing.Point(90, 0);
+            this.skinTabPage1.Name = "skinTabPage1";
+            this.skinTabPage1.Size = new System.Drawing.Size(593, 380);
+            this.skinTabPage1.TabIndex = 0;
+            this.skinTabPage1.TabItemImage = null;
+            this.skinTabPage1.Text = "skinTabPage1";
+            // 
+            // skinTabPage2
+            // 
+            this.skinTabPage2.BackColor = System.Drawing.Color.White;
+            this.skinTabPage2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.skinTabPage2.Location = new System.Drawing.Point(90, 0);
+            this.skinTabPage2.Name = "skinTabPage2";
+            this.skinTabPage2.Size = new System.Drawing.Size(593, 380);
+            this.skinTabPage2.TabIndex = 1;
+            this.skinTabPage2.TabItemImage = null;
+            this.skinTabPage2.Text = "skinTabPage2";
+            // 
+            // skinTabPage3
+            // 
+            this.skinTabPage3.BackColor = System.Drawing.Color.White;
+            this.skinTabPage3.Controls.Add(this.skinPushPanel1);
+            this.skinTabPage3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.skinTabPage3.Location = new System.Drawing.Point(90, 0);
+            this.skinTabPage3.Name = "skinTabPage3";
+            this.skinTabPage3.Size = new System.Drawing.Size(643, 424);
+            this.skinTabPage3.TabIndex = 2;
+            this.skinTabPage3.TabItemImage = null;
+            this.skinTabPage3.Text = "skinTabPage3";
+            // 
+            // skinPushPanel1
+            // 
+            this.skinPushPanel1.Items.AddRange(new CCWin.SkinControl.PushPanelItem[] {
+            this.pushPanelItem1,
+            this.pushPanelItem2,
+            this.pushPanelItem3,
+            this.pushPanelItem4,
+            this.pushPanelItem5});
+            this.skinPushPanel1.Location = new System.Drawing.Point(3, 3);
+            this.skinPushPanel1.Name = "skinPushPanel1";
+            this.skinPushPanel1.Size = new System.Drawing.Size(200, 418);
+            this.skinPushPanel1.TabIndex = 0;
+            // 
+            // pushPanelItem1
+            // 
+            this.pushPanelItem1.Border = System.Drawing.Color.FromArgb(((int)(((byte)(177)))), ((int)(((byte)(221)))), ((int)(((byte)(255)))));
+            this.pushPanelItem1.CaptionBackHover = System.Drawing.Color.FromArgb(((int)(((byte)(123)))), ((int)(((byte)(198)))), ((int)(((byte)(255)))));
+            this.pushPanelItem1.CaptionBackNormal = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(218)))), ((int)(((byte)(255)))));
+            this.pushPanelItem1.CaptionBackPressed = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(188)))), ((int)(((byte)(255)))));
+            this.pushPanelItem1.CaptionFont = new System.Drawing.Font("Microsoft YaHei UI", 9F);
+            this.pushPanelItem1.CaptionFore = System.Drawing.Color.Black;
+            this.pushPanelItem1.Name = "pushPanelItem1";
+            this.pushPanelItem1.Text = "pushPanelItem1";
+            // 
+            // pushPanelItem2
+            // 
+            this.pushPanelItem2.Border = System.Drawing.Color.FromArgb(((int)(((byte)(177)))), ((int)(((byte)(221)))), ((int)(((byte)(255)))));
+            this.pushPanelItem2.CaptionBackHover = System.Drawing.Color.FromArgb(((int)(((byte)(123)))), ((int)(((byte)(198)))), ((int)(((byte)(255)))));
+            this.pushPanelItem2.CaptionBackNormal = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(218)))), ((int)(((byte)(255)))));
+            this.pushPanelItem2.CaptionBackPressed = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(188)))), ((int)(((byte)(255)))));
+            this.pushPanelItem2.CaptionFont = new System.Drawing.Font("Microsoft YaHei UI", 9F);
+            this.pushPanelItem2.CaptionFore = System.Drawing.Color.Black;
+            this.pushPanelItem2.Name = "pushPanelItem2";
+            this.pushPanelItem2.Text = "pushPanelItem2";
+            // 
+            // pushPanelItem3
+            // 
+            this.pushPanelItem3.Border = System.Drawing.Color.FromArgb(((int)(((byte)(177)))), ((int)(((byte)(221)))), ((int)(((byte)(255)))));
+            this.pushPanelItem3.CaptionBackHover = System.Drawing.Color.FromArgb(((int)(((byte)(123)))), ((int)(((byte)(198)))), ((int)(((byte)(255)))));
+            this.pushPanelItem3.CaptionBackNormal = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(218)))), ((int)(((byte)(255)))));
+            this.pushPanelItem3.CaptionBackPressed = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(188)))), ((int)(((byte)(255)))));
+            this.pushPanelItem3.CaptionFont = new System.Drawing.Font("Microsoft YaHei UI", 9F);
+            this.pushPanelItem3.CaptionFore = System.Drawing.Color.Black;
+            this.pushPanelItem3.Name = "pushPanelItem3";
+            this.pushPanelItem3.Text = "pushPanelItem3";
+            // 
+            // pushPanelItem4
+            // 
+            this.pushPanelItem4.Border = System.Drawing.Color.FromArgb(((int)(((byte)(177)))), ((int)(((byte)(221)))), ((int)(((byte)(255)))));
+            this.pushPanelItem4.CaptionBackHover = System.Drawing.Color.FromArgb(((int)(((byte)(123)))), ((int)(((byte)(198)))), ((int)(((byte)(255)))));
+            this.pushPanelItem4.CaptionBackNormal = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(218)))), ((int)(((byte)(255)))));
+            this.pushPanelItem4.CaptionBackPressed = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(188)))), ((int)(((byte)(255)))));
+            this.pushPanelItem4.CaptionFont = new System.Drawing.Font("Microsoft YaHei UI", 9F);
+            this.pushPanelItem4.CaptionFore = System.Drawing.Color.Black;
+            this.pushPanelItem4.Name = "pushPanelItem4";
+            this.pushPanelItem4.Text = "pushPanelItem4";
+            // 
+            // pushPanelItem5
+            // 
+            this.pushPanelItem5.Border = System.Drawing.Color.FromArgb(((int)(((byte)(177)))), ((int)(((byte)(221)))), ((int)(((byte)(255)))));
+            this.pushPanelItem5.CaptionBackHover = System.Drawing.Color.FromArgb(((int)(((byte)(123)))), ((int)(((byte)(198)))), ((int)(((byte)(255)))));
+            this.pushPanelItem5.CaptionBackNormal = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(218)))), ((int)(((byte)(255)))));
+            this.pushPanelItem5.CaptionBackPressed = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(188)))), ((int)(((byte)(255)))));
+            this.pushPanelItem5.CaptionFont = new System.Drawing.Font("Microsoft YaHei UI", 9F);
+            this.pushPanelItem5.CaptionFore = System.Drawing.Color.Black;
+            this.pushPanelItem5.Controls.Add(this.skinPictureBox2);
+            this.pushPanelItem5.Controls.Add(this.skinPictureBox1);
+            this.pushPanelItem5.Name = "pushPanelItem5";
+            this.pushPanelItem5.Text = "pushPanelItem5";
+            // 
+            // skinPictureBox1
+            // 
+            this.skinPictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.skinPictureBox1.Location = new System.Drawing.Point(45, 27);
+            this.skinPictureBox1.Name = "skinPictureBox1";
+            this.skinPictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.skinPictureBox1.TabIndex = 0;
+            this.skinPictureBox1.TabStop = false;
+            // 
+            // skinPictureBox2
+            // 
+            this.skinPictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.skinPictureBox2.Location = new System.Drawing.Point(45, 83);
+            this.skinPictureBox2.Name = "skinPictureBox2";
+            this.skinPictureBox2.Size = new System.Drawing.Size(100, 50);
+            this.skinPictureBox2.TabIndex = 0;
+            this.skinPictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(749, 462);
+            this.Controls.Add(this.skinTabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.skinTabControl1.ResumeLayout(false);
+            this.skinTabPage3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.skinPushPanel1)).EndInit();
+            this.skinPushPanel1.ResumeLayout(false);
+            this.pushPanelItem5.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.skinPictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.skinPictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private CCWin.SkinControl.SkinTabControl skinTabControl1;
+        private CCWin.SkinControl.SkinTabPage skinTabPage1;
+        private CCWin.SkinControl.SkinTabPage skinTabPage2;
+        private CCWin.SkinControl.SkinTabPage skinTabPage3;
+        private CCWin.SkinControl.SkinPushPanel skinPushPanel1;
+        private CCWin.SkinControl.PushPanelItem pushPanelItem1;
+        private CCWin.SkinControl.PushPanelItem pushPanelItem2;
+        private CCWin.SkinControl.PushPanelItem pushPanelItem3;
+        private CCWin.SkinControl.PushPanelItem pushPanelItem4;
+        private CCWin.SkinControl.PushPanelItem pushPanelItem5;
+        private CCWin.SkinControl.SkinPictureBox skinPictureBox2;
+        private CCWin.SkinControl.SkinPictureBox skinPictureBox1;
+    }
+}
---
+++ Summer.CompetitiveTender.View/Form2.cs
@@ -0,0 +1,20 @@
+﻿using ChaseDream.EnterpriseLibraries.DSkinEngine;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Summer.CompetitiveTender.View
+{
+    public partial class Form2 : DSkinForm
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+    }
+}
